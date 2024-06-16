751a348f32b82bc0b0fa9ce65a63e9a723b8934c
Woo Jin Lee <100455176+wjin-lee@users.noreply.github.com>
2024-06-17 02:12:25 +1200

    tests: update deprecated tests. (#9)

--- src/checkout/pricing-rules/pricing-rule.interface.ts
+++ src/checkout/pricing-rules/pricing-rule.interface.ts
@@ -1,6 +1,16 @@
 import { ICartState } from "../cart-state";
 
 export interface IPricingRule {
+  /**
+   * Whether or not the condition for the pricing rule has been satisfied.
+   *
+   * @param cartState the current cart state.
+   */
   isConditionSatisfied(cartState: ICartState): boolean;
+
+  /**
+   * Applies the pricing rule to the given cart state.
+   * @param cartState cart state to apply the rule modifier to.
+   */
   apply(cartState: ICartState): ICartState;
 }
